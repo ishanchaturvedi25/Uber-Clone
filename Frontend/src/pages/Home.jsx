97905e195070fac7a1fae16b9a51b5b12ad56765
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2025-05-04 05:21:43 +0530

    Done some css changes for buttons

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -7,8 +7,8 @@ function Home() {
             <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1647692833760-83032ecf3e3e?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full'>
                 <img className='w-16 ml-8' src="https://freelogopng.com/images/all_img/1659768779uber-logo-white.png" alt="" />
                 <div className='bg-white py-4 px-4 pb-7'>
-                    <h2 className='text-2xl font-bold'>Get Started with Uber</h2>
-                    <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+                    <h2 className='text-[30px] font-bold'>Get Started with Uber</h2>
+                    <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5'>Continue</Link>
                 </div>
             </div>
         </div>
