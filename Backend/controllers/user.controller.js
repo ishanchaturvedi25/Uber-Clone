71675b588afecaa696ec04e272fd912aaf4384f7
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2025-05-03 20:05:06 +0530

    Added code for get profile and logout

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/user.model');
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
 
@@ -45,10 +46,41 @@ module.exports.loginUser = async (req, res, next) => {
             return res.status(401).json({ message: 'Invalid email or password' });
         
         const token = user.generateAuthToken();
-        res.status(200).json({ token });
+
+        res.cookie('token', token);
+        return res.status(200).json({ token });
     } catch (err) {
         console.log('Error while loging in: ', err);
         res.status(400).json({ message: err.message || 'Error while loging in' });
+    }
+
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+    try {
+        const user = await userModel.findOne({ _id: req.userId });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found'});
+        }
+
+        return res.status(200).json({ user });
+    } catch (error) {
+        return res.status(400).json({ message: error.message || 'Error while fetching user profile info' });
+    }
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+
+    try {
+        const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+        await blacklistTokenModel.create({ token: token });
+
+        res.clearCookie('token');
+        return res.status(200).json({ message: 'Logged out' });
+    } catch (error) {
+        return res.status(400).json({ message: error.message || 'Error while logging out' });
     }
 
 }
---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const blacklistTokenModel = require('../models/blacklistToken.model');
+
+module.exports.authUser = async (req, res, next) => {
+    
+    try {
+        const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const isBlacklisted = await blacklistTokenModel.findOne({ token });
+        console.log("blacklstoken: ", isBlacklisted);
+
+        if (isBlacklisted) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded._id;
+
+        return next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
