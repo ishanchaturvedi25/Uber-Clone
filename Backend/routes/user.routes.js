148c99fbd583eceb0b8ddecc77c45515c957ab00
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2025-05-03 19:11:01 +0530

    Added code for login

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -10,5 +10,10 @@ router.post('/register', [
     body('password').isLength({ min: 6 }).withMessage('Password must be atleast 6 character long')
 ], userController.registerUser);
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be atleast 6 character long')
+], userController.loginUser);
+
 
 module.exports = router;
